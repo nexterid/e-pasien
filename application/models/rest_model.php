b5969a3c5bd64a867a9e62e17ce19bded3fd9bd5
unknown <nexter.three@gmail.com>
2018-04-27 14:00:59 +0700

    alamat api

--- application/models/rest_model.php
+++ application/models/rest_model.php
@@ -7,7 +7,7 @@ class Rest_model extends CI_Model {
 
     public function __construct() {
         parent::__construct();
-        $this->API="http://192.168.0.229";
+        $this->API="http://api.rsudkraton.com";
     }
 
     public function sendpasien($data)
